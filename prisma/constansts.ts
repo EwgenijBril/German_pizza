5cdf674969e3a59f96f0ad35944cef952935865a
Evgeny <bril424@mail.ru>
2024-10-22 16:37:09 +0200

    created a postgreSQL in vercel

--- prisma/constansts.ts
+++ prisma/constansts.ts
@@ -105,3 +105,90 @@ export const ingredients = [
 			"https://cdn.dodostatic.net/static/Img/Ingredients/504371722e764fa9944b11708f079d1c.png",
 	},
 ].map((obj, index) => ({ id: index + 1, ...obj }));
+
+export const products = [
+	{
+		name: "Omelett mit Schinken und Pilzen",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE7970321044479C1D1085457A36EB.webp",
+		categoryId: 2,
+	},
+	{
+		name: "Peperoni-Omelett",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE94ECF33B0C46BA410DEC1B1DD6F8.webp",
+		categoryId: 2,
+	},
+	{
+		name: "Latte Macchiato",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp",
+		categoryId: 2,
+	},
+	{
+		name: "Schinken mit Käse",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp",
+		categoryId: 3,
+	},
+	{
+		name: "Hühnernuggets",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp",
+		categoryId: 3,
+	},
+	{
+		name: "Leberkartoffeln mit Soße 🌱",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp",
+		categoryId: 3,
+	},
+	{
+		name: "Dodster",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp",
+		categoryId: 3,
+	},
+	{
+		name: "Scharfsinniger Dodster 🌶️🌶️",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp",
+		categoryId: 3,
+	},
+	{
+		name: "Bananenmilchshake",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp",
+		categoryId: 4,
+	},
+	{
+		name: "Karamellapfelmilchshake",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp",
+		categoryId: 4,
+	},
+	{
+		name: "Milchshake 👶",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE796F93FB126693F96CB1D3E403FB.webp",
+		categoryId: 4,
+	},
+	{
+		name: "Irischer Cappuccino",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE7D61999EBDA59C10E216430A6093.webp",
+		categoryId: 5,
+	},
+	{
+		name: "Kokos Latte",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE7D61B19FA07090EE88B0ED347F42.webp",
+		categoryId: 5,
+	},
+	{
+		name: "Americano",
+		imageUrl:
+			"https://media.dodostatic.net/image/r:292x292/11EE7D61B044583596548A59078BBD33.webp",
+		categoryId: 5,
+	},
+];
